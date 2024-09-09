02c760e9266d7ea0f33f8209da0b84cc7180d8e8
Peezma <ezemartinex@gmail.com>
2024-09-09 16:10:43 -0300

    fix about us & add images

--- src/components/About-us/AboutUs.tsx
+++ src/components/About-us/AboutUs.tsx
@@ -5,88 +5,85 @@ const AboutUs = () => {
   return (
     <div>
       <NavbarComponent />
-      <div className="flex flex-col h-screen md:flex-row">
-        <div className="flex items-center justify-center flex-1 p-8 bg-gray-100">
-          <div>
-            <h1 className="mb-4 text-4xl font-bold">Sobre Nosotros</h1>
-            <p className="text-lg">
-              Bienvenidos a nuestro centro de spa, un lugar de relajación y
-              renovación. Nos dedicamos a ofrecer experiencias personalizadas
-              para el bienestar de nuestros clientes. Nuestro spa abrió sus
-              puertas en 2022 con la misión de ofrecer un oasis de tranquilidad
-              y bienestar a nuestros clientes. Desde el principio, hemos
-              trabajado para crear un espacio donde el cuerpo, la mente y el
-              espíritu puedan encontrar un equilibrio perfecto.
+      <div className="flex flex-col md:flex-row h-auto">
+        <div className="flex items-center justify-center flex-1 p-8 bg-AntiFlashWhite">
+          <div className="text-center md:text-left">
+            <h1 className="mb-4 text-3xl md:text-4xl font-bold">Sobre Nosotros</h1>
+            <img
+              src="/Logo-removebg-preview.png"
+              alt="Imagen del spa"
+              className="h-auto md:w-1/2 mx-auto md:mx-0 lg:hidden"
+            />
+            <p className="text-base md:text-lg mt-4">
+              Bienvenidos a nuestro centro de spa, un lugar de relajación y renovación. Nos dedicamos a ofrecer experiencias personalizadas para el bienestar de nuestros clientes. Nuestro spa abrió sus puertas en 2022 con la misión de ofrecer un oasis de tranquilidad y bienestar a nuestros clientes. Desde el principio, hemos trabajado para crear un espacio donde el cuerpo, la mente y el espíritu puedan encontrar un equilibrio perfecto.
             </p>
             <br />
-            <p className="text-lg">
-              Buscamos ser reconocidos como el spa líder en Resistencia,
-              innovando continuamente en el bienestar y cuidado personal para
-              proporcionar experiencias excepcionales a nuestros clientes.
+            <p className="text-base md:text-lg mt-4">
+              Buscamos ser reconocidos como el spa líder en Resistencia, innovando continuamente en el bienestar y cuidado personal para proporcionar experiencias excepcionales a nuestros clientes.
+            </p>
+            <br />
+            <p className="text-base md:text-lg mt-4">
+              Estamos dedicados a proteger nuestro entorno mediante prácticas ecológicas y sostenibles. Utilizamos productos orgánicos y biodegradables, y hemos implementado técnicas de ahorro de energía y agua en nuestras instalaciones. Creemos que el bienestar debe extenderse al planeta, y trabajamos constantemente para reducir nuestra huella ecológica mientras brindamos un servicio de alta calidad.
             </p>
           </div>
         </div>
-        <div className="flex items-center justify-center flex-1 bg-white">
+        <div className="hidden md:flex items-center justify-center flex-1 bg-AntiFlashWhite">
           <img
-            src="/public/Logo.jpeg"
+            src="/Logo-removebg-preview.png"
             alt="Imagen del spa"
             className="w-3/4 h-auto md:w-2/3"
           />
         </div>
       </div>
-      <div className="pt-8 text-gray-800 bg-gray-100">
+      <div className="pt-8 text-gray-800 bg-AntiFlashWhite">
         <div className="container px-4 mx-auto">
-          <h2 className="mb-8 text-3xl font-bold text-center">
-            Conoce a Nuestro Equipo
-          </h2>
+          <h2 className="text-2xl md:text-3xl font-bold text-center">Conoce a Nuestro Equipo</h2>
           <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
             <div className="p-6 bg-white rounded-lg shadow-lg">
               <img
-                src="/public/miembros/miembro-mujer.jpg"
+                src="/miembros/miembro-mujer.jpg"
                 alt="foto-miembro"
                 className="object-cover w-full h-40 rounded-t-lg"
               />
-              <h3 className="mt-4 text-xl font-semibold">Nombre Apellido</h3>
-              <p className="text-gray-600">Especialidad</p>
-              <p className="mt-2">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
-                animi pariatur incidunt accusantium quis dolore, exercitationem
-                recusandae enim eveniet reiciendis facilis odio officiis
-                perspiciatis ad id delectus? Illo, voluptatum placeat!
+              <h3 className="mt-4 text-lg md:text-xl font-semibold">Valeria Martínez</h3>
+              <p className="text-gray-600">Especialista en Tratamientos Faciales y Corporales</p>
+              <p className="mt-2 text-sm md:text-base">
+                Valeria ofrece tratamientos faciales y corporales personalizados para rejuvenecer y mejorar la textura de la piel. Su experiencia incluye peelings químicos y reducción de celulitis.
               </p>
             </div>
             <div className="p-6 bg-white rounded-lg shadow-lg">
               <img
-                src="/public/miembros/miembro-mujer.jpg"
+                src="/miembros/miembro-mujer2.jpg"
                 alt="foto-miembro"
                 className="object-cover w-full h-40 rounded-t-lg"
               />
-              <h3 className="mt-4 text-xl font-semibold">Nombre Apellido</h3>
-              <p className="text-gray-600">Especialidad</p>
-              <p className="mt-2">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Accusantium quibusdam nam autem veritatis dolorem reiciendis
-                ipsum, facilis sit id rerum animi error molestiae at atque
-                blanditiis cupiditate assumenda officia mollitia?
+              <h3 className="mt-4 text-lg md:text-xl font-semibold">Ana García</h3>
+              <p className="text-gray-600">Esteticista y Maquilladora Profesional</p>
+              <p className="mt-2 text-sm md:text-base">
+                Ana se especializa en maquillaje para eventos, tratamientos de belleza y cuidado de la piel. Realza la belleza natural con servicios como depilación, diseño de cejas y tintura de pestañas.
               </p>
             </div>
             <div className="p-6 bg-white rounded-lg shadow-lg">
               <img
-                src="/public/miembros/miembro-mujer.jpg"
+                src="/miembros/miembro-hombre.jpeg"
                 alt="foto-miembro"
                 className="object-cover w-full h-40 rounded-t-lg"
               />
-              <h3 className="mt-4 text-xl font-semibold">Nombre Apellido</h3>
-              <p className="text-gray-600">Especialidad</p>
-              <p className="mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-                rerum quos, molestiae odio aliquid, ratione aut molestias
-                blanditiis in maiores deleniti harum, dolores non id sint
-                officiis eum laudantium necessitatibus.
+              <h3 className="mt-4 text-lg md:text-xl font-semibold">Carlos Suárez</h3>
+              <p className="text-gray-600">Masajista Terapéutica</p>
+              <p className="mt-2 text-sm md:text-base">
+                Carlos ofrece masajes relajantes y terapéuticos para aliviar tensiones y mejorar el bienestar general. Su enfoque personalizado combina técnicas de masoterapia y aromaterapia.
               </p>
             </div>
           </div>
         </div>
+      </div>
+      <div className="bg-AntiFlashWhite p-8 text-center">
+        <p className="text-base md:text-lg">
+          <strong>Dirección:</strong> Av. 25 de Mayo 1234, Resistencia, Chaco<br />
+          <strong>Teléfono:</strong> 362 123 4567<br />
+          <strong>Horario:</strong> Lunes a Viernes de 9:00 a 20:00hs
+        </p>
       </div>
       <Footer />
     </div>
